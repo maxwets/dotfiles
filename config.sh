a90cb90ed8d80632e07dcddc84047ea7849882ba
maxwets <max.wets@gmail.com>
2018-02-09 15:58:17 +0100

    Config file can now be used on a root user

--- config.sh
+++ config.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # This file just creates a link for all the dotfiles in the /home/$USER/. 
-rm /home/`whoami`/.bashrc
-rm /home/`whoami`/.vimrc
-rm /home/`whoami`/.tmux.conf
-link .bashrc /home/`whoami`/.bashrc
-link .vimrc /home/`whoami`/.vimrc
-link .tmux.conf /home/`whoami`/.tmux.conf
+if [ `whoami` -nq "root" ]; then
+	rm /home/`whoami`/.bashrc
+	rm /home/`whoami`/.vimrc
+	rm /home/`whoami`/.tmux.conf
+	link .bashrc /home/`whoami`/.bashrc
+	link .vimrc /home/`whoami`/.vimrc
+	link .tmux.conf /home/`whoami`/.tmux.conf
+else 
+	rm /root/.bashrc
+	rm /root/.vimrc
+	rm /root/.tmux.conf
+	link .bashrc /root/.bashrc
+	link .vimrc /root/.vimrc
+	link .tmux.conf /root/.tmux.conf
+fi
 
 # After creating all links, we need to exit and launch a new terminal
 killall tmux
