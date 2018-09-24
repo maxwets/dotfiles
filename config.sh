4ee331cbc3088b46daf0aea68d67537d5e8827ff
maxwets <max.wets@gmail.com>
2018-09-24 22:08:07 +0200

    Update config.sh so now it works on any machine.

--- config.sh
+++ config.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-# This file just creates a link for all the dotfiles in the /home/$USER/. 
-if [ `whoami` -nq "root" ]; then
-	rm /home/`whoami`/.bashrc
-	rm /home/`whoami`/.vimrc
-	rm /home/`whoami`/.tmux.conf
-	link .bashrc /home/`whoami`/.bashrc
-	link .vimrc /home/`whoami`/.vimrc
-	link .tmux.conf /home/`whoami`/.tmux.conf
-else 
-	rm /root/.bashrc
-	rm /root/.vimrc
-	rm /root/.tmux.conf
-	link .bashrc /root/.bashrc
-	link .vimrc /root/.vimrc
-	link .tmux.conf /root/.tmux.conf
-fi
+# This file just creates a link for all the dotfiles in the $HOME directory. 
+rm $HOME/.bashrc
+rm $HOME/.vimrc
+rm $HOME/.tmux.conf
+link .bashrc $HOME/.bashrc
+link .vimrc $HOME/.vimrc
+link .tmux.conf $HOME/.tmux.conf
 
 # After creating all links, we need to exit and launch a new terminal
 killall tmux
