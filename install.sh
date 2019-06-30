7950cb05364f1d396b30a8bc2cffa211e66e4b7d
Max Wets <max.wets@gmail.com>
2019-06-30 15:07:20 +0200

    Updated install script.

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 # install evil-mode for emacs
 git clone https://github.com/emacs-evil/evil ~/.emacs.d/evil
+
+# install gruvbox for emacs
+[ -d ~/.emacs.d/themes ] || mkdir ~/.emacs.d/themes
+curl -L https://raw.github.com/Greduan/emacs-theme-gruvbox/master/gruvbox-theme.el > ~/.emacs.d/themes/gruvbox-theme.el
