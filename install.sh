15bc3a8f920cc70a0a62c38d37133ce8753816cf
Max Wets <max.wets@gmail.com>
2022-07-29 18:46:13 +0200

    More programs

--- install.sh
+++ install.sh
@@ -29,3 +29,4 @@ fi
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
