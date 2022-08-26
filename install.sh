81fc1cbf13e9ee5a67383266ae99ca06fd4e84f5
Max Wets <max.wets@gmail.com>
2022-08-26 16:59:30 +0200

    progs + install script modif + i3 config modif

--- install.sh
+++ install.sh
@@ -10,23 +10,24 @@ then
 		pacman -Syu;
 		pacman -R noto-fonts-emoji
 		pacman -Sy $(cat progs/pacman.list);
-	elif [[ $1 == "yay" ]]; then
+	elif [[ $1 == "aur" ]]; then
 		yay -Syu $(cat progs/aur.list)
 	elif [[ $1 == "pip" ]]; then
 		pip install $(cat progs/pip.list)
-	fi
-
-	if [ ! -d /tmp/dmenu ]; then
-		git clone https://github.com/maxwets/dmenu.git /tmp/dmenu
-		make install -C /tmp/dmenu
-	fi
-
-	if [ ! -d /tmp/st ]; then
-		git clone https://github.com/maxwets/st.git /tmp/st
-		make install -C /tmp/st
+	else
+		if [ ! -d /tmp/dmenu ]; then
+			git clone https://github.com/maxwets/dmenu.git /tmp/dmenu
+			make install -C /tmp/dmenu
+		fi
+		if [ ! -d /tmp/st ]; then
+			git clone https://github.com/maxwets/st.git /tmp/st
+			make install -C /tmp/st
+		fi
 	fi
 fi
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
+if [ $# -eq 0 ]; then
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+	curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
+fi
